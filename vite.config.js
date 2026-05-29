7e52509223ed6ad60f5cd0c4f6080a5523429db4
Shedrack Joshua <shedrackjoshua234@gmail.com>
2026-05-29 16:16:32 +0100

    Updated content

--- vite.config.js
+++ vite.config.js
@@ -32,6 +32,6 @@ export default defineConfig(({ command }) => {
   // Production config (GitHub Pages)
   return {
     ...common,
-    base: process.env.VERCEL ? '/learnwitheasetutors/' : '/'
+    base: '/learnwitheasetutors/'
   }
 });

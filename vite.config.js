332c8673c7c603cda2a3f8758b66a3668acd69e2
Shedrack Joshua <shedrackjoshua234@gmail.com>
2026-05-30 14:39:29 +0100

    Updated content

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,37 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const isVercel = process.env.VERCEL
+export default defineConfig(({ command }) => {
+  const common = {
+    plugins: [vue()]
+  }
 
-export default defineConfig({
-  plugins: [vue()],
-  base: isVercel ? '/' : '/learnwitheasetutors/',
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: path => path
+  if (command === 'serve') {
+    // Development config
+    return {
+      ...common,
+      server: {
+        port: 5173,
+        proxy: {
+          '/api': {
+            target: 'http://localhost:5000',
+            changeOrigin: true,
+            secure: false,
+            rewrite: path => path
+          }
+        },
+        hmr: {
+          protocol: 'ws',
+          host: 'localhost',
+          port: 5173
+        }
       }
-    },
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost',
-      port: 5173
     }
   }
-})
+
+  // Production config (GitHub Pages)
+  return {
+    ...common,
+    base: process.env.VERCEL ? '/' : '/learnwitheasetutors/'
+  }
+});
